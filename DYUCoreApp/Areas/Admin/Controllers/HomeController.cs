6889b43e51e72efd8c1fa41b10a258e71b7db4f3
jtquangduy <justinquangduy@gmail.com>
2022-01-10 00:36:37 +0700

    Login for admin page

--- DYUCoreApp/Areas/Admin/Controllers/HomeController.cs
+++ DYUCoreApp/Areas/Admin/Controllers/HomeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DYUCoreApp.Areas.Admin.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         [Area("Admin")]
--- DYUCoreApp/Startup.cs
+++ DYUCoreApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using System;
 using TeduCoreApp.Data.EF;
 using TeduCoreApp.Data.EF.Repositories;
@@ -70,12 +72,13 @@ namespace DYUCoreApp
 
             services.AddTransient<IProductCategoryService, ProductCategoryService>();
 
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/dyu-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
